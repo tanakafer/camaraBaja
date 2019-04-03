09073431ef83dc0e6fa348bc2c4ecc762e793c32
Fernando Rodriguez <fernando.rodriguez@ohl.es>
2019-04-03 22:25:21 +0200

    Los parámetros de pandoc se han pasado al script, para evitar que se vean en la representación de github del README.md

--- README.md
+++ README.md
@@ -1,20 +1,4 @@
----
-title: "Tipología y ciclo de vida de los datos"
-subtitle: "Práctica 1"
-author: [Fernando Rodríguez López]
-date: "08/04/2018"
-subject: "webscraping"
-keywords: [webscraping, UOC]
-titlepage: true
-toc-own-page: true
-titlepage-text-color: "000078"
-titlepage-rule-color: "000078"
-titlepage-rule-height: 2
-uoc: "images/logo-uoc-default.png"
-logo: "images/candidates.png"
-header-right: "Fernando Rodríguez López"
-footer-left: "08/04/2018"
-...
+
 
 # Título del dataset
 
@@ -23,6 +7,7 @@ Diputados de España (DCE)
 # Subtítulo del dataset
 
 Recopilación de todos los diputados del congreso de los diputados de España desde su constitución en 1977.
+
 # Imagen
 
 # Contexto
--- pandoc.sh
+++ pandoc.sh
@@ -5,8 +5,23 @@ output=practica1
 # Creamos el fichero pdf
 docker run --name temppandoc -v \
           "$directory":/source jagregory/pandoc \
+          --variable title="Tipología y ciclo de vida de los datos" \
+          --variable subtitle="Práctica 1" \
+          --variable author="[Fernando Rodríguez López]" \
+          --variable date="08/04/2018" \
+          --variable subject="webscraping" \
+          --variable keywords="[webscraping, UOC]" \
+          --variable titlepage=true \
+          --variable toc-own-page=true \
+          --variable titlepage-text-color="000078" \
+          --variable titlepage-rule-color="000078" \
+          --variable titlepage-rule-height=2 \
+          --variable uoc="images/logo-uoc-default.png" \
+          --variable logo="images/candidates.png" \
+          --variable header-right="Fernando Rodríguez López" \
+          --variable footer-left="08/04/2018" \
           -f markdown \
-          -t latex README.md \
+          -t latex  README.md \
           --template uoc.tex \
           --listing \
           -o $output.pdf
@@ -22,3 +37,22 @@ docker run --name temppandoc -v \
           -o $output.docx
 docker rm temppandoc
 echo "Creado fihero docx"
+
+#
+# ---
+# title: "Tipología y ciclo de vida de los datos"
+# subtitle: "Práctica 1"
+# author: [Fernando Rodríguez López]
+# date: "08/04/2018"
+# subject: "webscraping"
+# keywords: [webscraping, UOC]
+# titlepage: true
+# toc-own-page: true
+# titlepage-text-color: "000078"
+# titlepage-rule-color: "000078"
+# titlepage-rule-height: 2
+# uoc: "images/logo-uoc-default.png"
+# logo: "images/candidates.png"
+# header-right: "Fernando Rodríguez López"
+# footer-left: "08/04/2018"
+# ...
